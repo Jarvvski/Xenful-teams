65eb1b6a598384382aa849aadc9b8b3fc0363f7f
Adam D Jarvis <adam.jarvis2012@gmail.com>
2017-07-18 11:39:14 +0100

    Stop load of teams if user doesn't even have perms to view the dash

--- library/ADR/ControllerPublic/Dashboard.php
+++ library/ADR/ControllerPublic/Dashboard.php
@@ -5,12 +5,20 @@ class Teams_ControllerPublic_Dashboard extends Teams_ControllerPublic_Abstract
 
 	public function actionIndex()
 	{
+        // If cannot view, escape
+        // and save on team load
+        if (!XenForo_Visitor::getInstance()->hasPermission('XenFullTeams', 'viewDash'))
+        {
+            throw $this->getNoPermissionResponseException();
+        }
+
 		return $this->responseReroute(__CLASS__, 'dashboard');
 	}
 
 	public function actionDashboard()
 	{
 		// TODO display dashboard for user
+		
 	}
 
 	public function actionCreate()
