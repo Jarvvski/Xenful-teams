5b63ee3f99f1d5b03d2313b8ca565f77e31c0756
Adam D Jarvis <adam.jarvis2012@gmail.com>
2017-07-28 12:41:48 +0100

    Role DataWriter updated to include managed_team_ids

--- library/Teams/DataWriter/Role.php
+++ library/Teams/DataWriter/Role.php
@@ -5,15 +5,16 @@ class Teams_DataWriter_Role extends XenForo_DataWriter
 	protected function _getFields()
 	{
 		return array(
-			'xf_teams_relations' => array(
-				'relation_id' => array('type' => self::TYPE_UINT, 'autoIncrement' => true),
+			'xf_teams_roles' => array(
+				'role_id' => array('type' => self::TYPE_UINT, 'autoIncrement' => true),
 				'team_id' => array('type' => self::TYPE_INT, 'required' => true),
 				'user_id' => array('type' => self::TYPE_INT),
 				'username' => array('type' => self::TYPE_STRING, 'maxLength' => 240),
 				'role_title' => array('type' => self::TYPE_STRING, 'maxLength' => 240, 'required' => true),
 				'remark' => array('type' => self::TYPE_STRING, 'maxLength' => 240),
-				'admin' => array('type' => self::TYPE_BOOLEAN, 'required' => true, 'default' => 0),
-				'mod' => array('type' => self::TYPE_BOOLEAN, 'required' => true, 'default' => 0),
+				'managed_team_ids' => array('type' => self::TYPE_UNKNOWN, 'required' => true, 'default' => '',
+					'verification' => array('$this', '_verifyManagedTeamIds')
+				),
 				'hierarchy' => array('type' => self::TYPE_INT, 'required' => true),
 				'primary' => array('type' => self::TYPE_BOOLEAN, 'required' => true, 'default' => 1)
 			)
@@ -22,17 +23,32 @@ class Teams_DataWriter_Role extends XenForo_DataWriter
 
 	protected function _getExistingData($data)
 	{
-		if (!$id = $this->_getExistingPrimaryKey($data, 'relation_id'))
+		if (!$id = $this->_getExistingPrimaryKey($data, 'role_id'))
 		{
 			return false;
 		}
 
-		return array('xf_teams_relations' => $this->_getRoleModel()->getRoleByIdSimple($id));
+		return array('xf_teams_roles' => $this->_getRoleModel()->getRoleByIdSimple($id));
 	}
+
+    protected function _verifyManagedTeamIds(&$teamIds)
+    {
+        if (!is_array($teamIds))
+        {
+            $teamIds = preg_split('#,\s*#', $teamIds);
+        }
+
+        $teamIds = array_map('intval', $teamIds);
+        $teamIds = array_unique($teamIds);
+        sort($teamIds, SORT_NUMERIC);
+        $teamIds = implode(',', $teamIds);
+
+        return true;
+    }
 
 	protected function _getUpdateCondition($tableName)
 	{
-		return 'relation_id = ' . $this->_db->quote($this->getExisting('relation_id'));
+		return 'role_id = ' . $this->_db->quote($this->getExisting('role_id'));
 	}
 
 	protected function _getRoleModel()
